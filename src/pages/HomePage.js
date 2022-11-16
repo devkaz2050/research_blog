f799d324f8067ccc4b694cda9cb4c68b1264fe8b
altynbek2003 <altynbek2003"gmail.com>
2022-11-16 19:11:38 +0600

    first commit7

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/infographic.png';
+import logo from '../images/info.png';
 const HomePage = () => (
     <>
     <h1>Hello, welcome to my DataScience blog!</h1>
@@ -9,7 +9,7 @@ const HomePage = () => (
         As part of the course in Research Project Management, this web page will discuss my approach, how research results will be evaluated,
          what research methodology and related tools will be used.
     </p>
-    <img src={logo} alt="Logo" />
+    <img src={logo} className="photo" alt="Logo" />
     </>
 );
 
