7ae7f955c12c2bcc846c07389440b68bf01c10e3
altynbek2003 <altynbek2003"gmail.com>
2022-11-16 18:45:03 +0600

    commit3

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -5,20 +5,18 @@ const articles = [
         content: [
             `My Research Project Roadmap will consist of several stages:
 
-            •	Identifying the Research Problem and Overview;
-            •	Conducting Literature Review;
-            •	Setting Research Aims and Questions;
-            •	Providing Research Design and Methodology;
-            •	Data Collection Methods;
-            •	Data Analysis;
-            •	Conclusions and completing research project.
+            (1) Identifying the Research Problem and Overview;
+            (2) Conducting Literature Review;
+            (2) Setting Research Aims and Questions;
+            (3) Providing Research Design and Methodology;
+            (4) Data Collection Methods;
+            (5) Data Analysis;
+            (6) Conclusions and completing research project.
             `,
-            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `Identifying the Research Problem and Overview;
+
+            The research topic is dedicated to the utilization of machine learning methods - reinforcement learning to identify anomalies in patterns in time series. Research Problem - Anomaly Detection in Time Series with Machine Learning Algorithms
+            `,
             `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
             Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
             nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
