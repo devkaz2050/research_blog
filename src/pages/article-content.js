2b355c8a6d8f0b5d6e984559e17c4da2ebed59da
altynbek2003 <altynbek2003"gmail.com>
2022-11-16 18:53:56 +0600

    first commit5

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -13,16 +13,52 @@ const articles = [
             (5) Data Analysis;
             (6) Conclusions and completing research project.
             `,
-            `Identifying the Research Problem and Overview;
+            `1 - Identifying the Research Problem and Overview;
 
-            <br> The research topic is dedicated to the utilization of machine learning methods - reinforcement learning to identify anomalies in patterns in time series. Research Problem - Anomaly Detection in Time Series with Machine Learning Algorithms
+           The research topic is dedicated to the utilization of machine learning methods - reinforcement learning to identify anomalies in patterns in time series. Research Problem - Anomaly Detection in Time Series with Machine Learning Algorithms
             `,
-            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `2 - Conducting Literature Review; Conducting a literature review is one of the important and extensive stages of the research process. Examines current research in the selected subject area, which provides access to more knowledge and identifies gaps in research that may not have been explored until now.
+             Conducting a literature review allows to enlarge more knowledge about a given research problem and become familiar with the approaches, concepts of other researchers when conducting research on the problem, as well as what methodology was used to solve this problem.
+            `,
+            `3 - Setting Research Aims and Questions;
+
+            After describing the research problem and conducting the literature review, it is necessary to define the goals and questions of the research. 
+            
+            The research aims and research questions may change cyclically as research project is conducted through multiple attempts or revisions. Therefore, this process will be coordinated with the supervisor in terms of obtaining the final version of the research questions and objectives.
+            
+            From a clear setting of research aims and questions, it depends on what results and outcomes need to be achieved, which affects the progress of the research project for 3 months.          
+           `,
+           `4 - Providing Research Design and Methodology;
+
+
+           The research methodology will be based on a quantitative approach, and observational data will be used in the design of the study.
+           
+           The quantitative approach is the analysis of events by collecting measurable data and applying statistical and mathematical methodologies.
+           
+           A quantitative approach allows to provide more accurate and reliable data for meaningful results.
+           
+           However, the quantitative approach may miss or fail to capture broader relationships between data because it aims to find specific statistical relationships.
+           
+           This process is a very important step in the process of managing a research project, the success of which depends on the right choice of methodology and approaches of machine learning algorithms. The duration of this process will be 2-3 weeks and should be completed by February 15, 2023.
+           
+           .          
+          `,
+          `5 - Data Collection;
+
+          At this stage in the research process, it is necessary to decide on a sample of preliminary data and data sources. The data type is numeric data, time series.
+                   
+         `
+          ,
+          `6 - Data Analysis;
+
+          At this stage of the research process, the analysis and processing of the collected information is carried out to obtain patterns of anomalies in time series. Data analysis will include statistical analysis with appropriate charts and graphs or tables.
+              
+         `,
+         `7 - Conclusions and completing research project.
+
+         After the data analysis is completed, it is necessary to provide appropriate conclusions on the achievement of the goals and objectives of the study. In the final part of the study, it is necessary to substantiate why the objectives of the study were achieve
+         
+        `,
         ]
     }
 ];
