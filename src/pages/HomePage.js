cda84c9ea5968a4438ddd2139c1243ef6ae8c823
altynbek2003 <altynbek2003"gmail.com>
2022-11-19 13:27:32 +0600

    commit 4

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,8 @@ const HomePage = () => (
         As part of the course in PROM05 Research Project Management, this web blog page will outline my approach, how research results will be evaluated,
          what research methodology and related tools will be used.
     </p>
- 
+    <div>Best regrads,</div>
+    <div>Altynbek.</div>
     </>
 );
 
